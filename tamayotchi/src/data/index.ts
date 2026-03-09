69cc8d8d93cb10c992ce0f4d827763759e6f031a
Juan Tamayo <juancyepest@gmail.com>
2026-03-09 13:20:30 -0500

    fix

--- tamayotchi/src/data/index.ts
+++ tamayotchi/src/data/index.ts
@@ -1,7 +1,14 @@
-import rawData from "./data.json";
-import { CurrencyCode, InvestmentData } from "../types";
+import rawData from "../../../priv/data/data.json";
+import { CurrencyCode, InvestmentData, InvestmentProvider } from "../types";
 
-export const investmentData: InvestmentData = Object.entries(rawData).reduce(
+type RawInvestmentProvider = {
+  content: InvestmentProvider["content"];
+  currencyCode: keyof typeof CurrencyCode;
+};
+
+const dataByProvider = rawData as Record<string, RawInvestmentProvider>;
+
+export const investmentData: InvestmentData = Object.entries(dataByProvider).reduce(
   (acc, [provider, data]) => ({
     ...acc,
     [provider]: {
