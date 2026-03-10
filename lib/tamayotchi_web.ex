a9e88c9bdf06a87d635749745dd619a5d9886cee
Juan Tamayo <juancyepest@gmail.com>
2026-03-10 09:44:10 -0500

    remove v1

--- lib/tamayotchi_web.ex
+++ lib/tamayotchi_web.ex
@@ -17,7 +17,7 @@ defmodule TamayotchiWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt tamagotchi.png)
 
   def router do
     quote do
