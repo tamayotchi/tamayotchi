7bfc02690b3b1cb51828bd4f45efa50a0cbb6696
Juan Tamayo <juancyepest@gmail.com>
2026-08-29 18:17:20 -0500

    revert

--- assets/js/goatcounter.js
+++ assets/js/goatcounter.js
@@ -1,6 +1,5 @@
 window.goatcounter = window.goatcounter || {}
 window.goatcounter.endpoint = "https://tamayotchi.goatcounter.com/count"
-window.goatcounter.path = (path) => `${window.location.host}${path}`
 
 // Load the self-hosted GoatCounter client after configuring its endpoint.
 void import("../vendor/goatcounter")
